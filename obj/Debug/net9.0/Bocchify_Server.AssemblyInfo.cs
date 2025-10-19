ead447d9c0ba17c2f91169e9fffa9ca8c9a7fa83
raula09 <davitoniani0602@gmail.com>
2025-10-19 16:57:37 +0400

    Optimised AI

--- obj/Debug/net9.0/Bocchify_Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/Bocchify_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bocchify_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d9fedb14ec65acf78b8d25b5b35ccec7fa9755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e85e162347fa0f0f7a6dadd1f9929fe2f35169e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bocchify_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bocchify_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
